d7cf0f4c4cb393a58a6c679954c1e4918baf1058
Mauricio Araya <103547473+MauricioAraya21@users.noreply.github.com>
2024-09-30 23:11:05 -0300

    Commit configuracion cuenta
    Se realizo el formulario de usuario para que pueda editar sus datos personales

--- crux/funciones/mensaje.php
+++ crux/funciones/mensaje.php
@@ -7,3 +7,15 @@ function mensaje($titulo, $mensaje, $tipo = "info",$icono = "info-circle") {
     </div>
     <?
 }
+
+function mensaje_exito($titulo, $mensaje) {
+    ?>
+    <div class="alert alert-success" role="alert" style="text-align: center;">
+        <h3 class="alert-heading">
+            <img src="path_to_gif/check.gif" alt="Éxito" style="width: 50px; height: 50px;">
+            &nbsp;<?=$titulo?>
+        </h3>
+        <p><?=$mensaje?></p>
+    </div>
+    <?php
+}
